a988b7865f457b5495112f1a84cd2ae37ee78115
Igor Djokic <zigimanx@gmail.com>
2022-05-13 20:58:56 +0200

    Predavanja 13.05 Git Zadatak1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,7 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git clone,git status,git add,git commit -m,git push,touch,git restore --staged,git add.");
+    }
+}
