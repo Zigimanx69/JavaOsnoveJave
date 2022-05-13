59f1b5fbf43c84ed253a018761f6c77331e3150a
Igor Djokic <zigimanx@gmail.com>
2022-05-13 20:46:54 +0200

    Predavanje 13.05 Git Zadatak1

---
+++ osnove_jave/src/main/java/tekstovi_zadataka/txt/Git_Zadatak1.java
@@ -0,0 +1,13 @@
+package tekstovi_zadataka.txt;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println(" git clone ,git status,git add, git commit -m, git push,touch;  git restore --staged  ");
+
+
+
+
+
+
+    }
+}
