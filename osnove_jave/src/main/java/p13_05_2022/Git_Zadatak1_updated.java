8bcfe98249cc26b83f58fda929de8ead4b7f9a34
Igor Djokic <zigimanx@gmail.com>
2022-05-13 21:47:24 +0200

    Deleted class

--- osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1_updated.java
+++
@@ -1,7 +0,0 @@
-package p13_05_2022;
-
-public class Git_Zadatak1_updated {
-    public static void main(String[] args) {
-        System.out.println("git clone,git status,git add,git commit -m,git push,touch,git restore --staged,git add.");
-    }
-}
